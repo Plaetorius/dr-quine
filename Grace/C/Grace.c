04c1d613998f6d37491c5299f309d341abb8d202
Tag6362 <tag6362@gmail.com>
2023-10-21 21:51:32 +0200

    Grace done

--- Grace/C/Grace.c
+++ Grace/C/Grace.c
@@ -1,4 +1,7 @@
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-int main() {char *string="#include <fcntl.h>%c#include <unistd.h>%c#include <stdio.h>%cint main() {char *string=%c%s%c; dprintf(open(%cGrace_kid.c%c, O_WRONLY | O_CREAT, 0644), string, 10, 10, 10, 34, string, 34, 34, 34); return 0;}"; dprintf(open("Grace_kid.c", O_WRONLY | O_CREAT, 0644), string, 10, 10, 10, 34, string, 34, 34, 34); return 0;}
+#define FCNTL <fcntl.h>
+#define STDIO <stdio.h>
+//This is a comment
+#define MAIN int main() {char *string="#define FCNTL <fcntl.h>%c#define STDIO <stdio.h>%c//This is a comment%c#define MAIN int main() {char *string=%c%s%c; dprintf(open(%cGrace_kid.c%c, O_WRONLY | O_CREAT, 0644), string, 10, 10, 10, 34, string, 34, 34, 34, 10, 10, 10); return 0;}%c#include FCNTL%c#include STDIO%cMAIN"; dprintf(open("Grace_kid.c", O_WRONLY | O_CREAT, 0644), string, 10, 10, 10, 34, string, 34, 34, 34, 10, 10, 10); return 0;}
+#include FCNTL
+#include STDIO
+MAIN
