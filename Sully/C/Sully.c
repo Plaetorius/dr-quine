75640371b5d658314e3e9aed2f9d0c4cdd95e1f9
Tom GERNEZ <tgernez@student.42.fr>
2023-10-23 06:27:37 +0200

    Sully

--- Sully/C/Sully.c
+++ Sully/C/Sully.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <fcntl.h>
-
 int main()
 {
-	dprintf(open("second_file.c", O_WRONLY | O_CREAT, 0644), "#include <stdio.h>%cint main(){printf(%cbonjour%c);return 0;}", 10, 34, 34);
-	system("gcc second_file.c -o second_file");
-	execv("./second_file", NULL);
+	int i = 5;
+	if (i < 1)
+		return 1;
+	char name[] = "Sully_ .c";
+	char comp[] = "gcc Sully_ .c -o Sully_ ; ./Sully_ ";
+	name[6] = i + 47;
+ 	comp[10] = i + 47;
+ 	comp[22] = i + 47;
+ 	comp[33] = i + 47;
+ 	char *a = "#include <stdio.h>%c#include <stdlib.h>%c#include <fcntl.h>%cint main()%c{%c%cint i = %d;%c%cif (i < 1)%c%c%creturn 1;%c%cchar name[] = %cSully_ .c%c;%c%cchar comp[] = %cgcc Sully_ .c -o Sully_ ; ./Sully_ %c;%c%cname[6] = i + 47;%c %ccomp[10] = i + 47;%c %ccomp[22] = i + 47;%c %ccomp[33] = i + 47;%c %cchar *a = %c%s%c;%c%cdprintf(open(name, O_WRONLY | O_CREAT, 0644), a, 10, 10, 10, 10, 10, 9, i - 1, 10, 9, 10, 9, 9, 10, 9, 34, 34, 10, 9, 34, 34, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 34, a, 34, 10, 9, 10, 9, 10, 9, 10); %c%csystem(i < 1 ? NULL : comp);%c%creturn 0;%c}";
+	dprintf(open(name, O_WRONLY | O_CREAT, 0644), a, 10, 10, 10, 10, 10, 9, i - 1, 10, 9, 10, 9, 9, 10, 9, 34, 34, 10, 9, 34, 34, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 34, a, 34, 10, 9, 10, 9, 10, 9, 10); 
+	system(i < 1 ? NULL : comp);
 	return 0;
 }
