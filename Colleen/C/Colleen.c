1a2ae6f167640b40b12fb63bc6b100966a1f0819
Tom GERNEZ <tgernez@student.42.fr>
2023-10-23 03:59:33 +0200

    Actualize comment style

--- Colleen/C/Colleen.c
+++ Colleen/C/Colleen.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
 int rline(){return 10;}
-// This is an outside comment
-int main(){/*This an inside commnent*/char *a="#include <stdio.h>%cint rline(){return 10;}%c// This is an outside comment%cint main(){/*This an inside commnent*/char *a=%c%s%c;printf(a, rline(), rline(), rline(), 34, a, 34);return 0;}";printf(a, rline(), rline(), rline(), 34, a, 34);return 0;}
+/*
+	This is an outside comment
+*/
+int main(){
+/*
+	This an inside comment
+*/
+char *a="#include <stdio.h>%cint rline(){return 10;}%c/*%c%cThis is an outside comment%c*/%cint main(){%c/*%c%cThis an inside comment%c*/%cchar *a=%c%s%c;printf(a, rline(), rline(), rline(), 9, rline(), rline(), rline(), rline(), 9, rline(), rline(), 34, a, 34);return 0;}";printf(a, rline(), rline(), rline(), 9, rline(), rline(), rline(), rline(), 9, rline(), rline(), 34, a, 34);return 0;}
--- Grace/C/Grace.c
+++ Grace/C/Grace.c
@@ -1,7 +1,9 @@
 #define FCNTL <fcntl.h>
 #define STDIO <stdio.h>
-//This is a comment
-#define MAIN int main() {char *string="#define FCNTL <fcntl.h>%c#define STDIO <stdio.h>%c//This is a comment%c#define MAIN int main() {char *string=%c%s%c; dprintf(open(%cGrace_kid.c%c, O_WRONLY | O_CREAT, 0644), string, 10, 10, 10, 34, string, 34, 34, 34, 10, 10, 10); return 0;}%c#include FCNTL%c#include STDIO%cMAIN"; dprintf(open("Grace_kid.c", O_WRONLY | O_CREAT, 0644), string, 10, 10, 10, 34, string, 34, 34, 34, 10, 10, 10); return 0;}
+/*
+	This is a comment
+*/
+#define MAIN int main() {char *string="#define FCNTL <fcntl.h>%c#define STDIO <stdio.h>%c/*%c%cThis is a comment%c*/%c#define MAIN int main() {char *string=%c%s%c; dprintf(open(%cGrace_kid.c%c, O_WRONLY | O_CREAT, 0644), string, 10, 10, 10, 9, 10, 10, 34, string, 34, 34, 34, 10, 10, 10); return 0;}%c#include FCNTL%c#include STDIO%cMAIN"; dprintf(open("Grace_kid.c", O_WRONLY | O_CREAT, 0644), string, 10, 10, 10, 9, 10, 10, 34, string, 34, 34, 34, 10, 10, 10); return 0;}
 #include FCNTL
 #include STDIO
 MAIN
