d1c0e5d317eb55d5f745e825090aa0099def36ed
Tom GERNEZ <tgernez@student.42.fr>
2023-12-06 13:11:53 +0100

    better sully. might make it so we can start at any given integer using sprintf

--- Sully/C/Sully.c
+++ Sully/C/Sully.c
@@ -4,14 +4,13 @@
 
 int main() {
 	int i = 5;
-	if (i < 1) return 1;
 	char name[] = "Sully_ .c";
 	char comp[] = "clang Sully_ .c -o Sully_ ; ./Sully_ ";
 	name[6] = i + 48;
 	comp[12] = i + 48;
 	comp[25] = i + 48;
 	comp[36] = i + 48;
-	char *a = "#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <fcntl.h>%1$cint main()%1$c{%1$cint i = %2$d;%1$cif (i < 1) return 1;%1$cchar name[] = %3$cSully_ .c%3$c;%1$cchar comp[] = %3$cclang Sully_ .c -o Sully_ ; ./Sully_ %3$c;%1$cname[6] = i + 48; comp[12] = i + 48; comp[25] = i + 48; comp[36] = i + 48; %1$cchar *a = %3$c%4$s%3$c;%1$cprintf(%3$c%%s%3$c, comp);%1$cdprintf(open(name, O_WRONLY | O_CREAT, 0644), a, 10, i - 1, 34, a);%1$csystem(comp);%1$c}";
+	char *a = "#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <fcntl.h>%1$cint main()%1$c{%1$cint i = %2$d;%1$cchar name[] = %3$cSully_ .c%3$c;%1$cchar comp[] = %3$cclang Sully_ .c -o Sully_ ; ./Sully_ %3$c;%1$cname[6] = i + 48; comp[12] = i + 48; comp[25] = i + 48; comp[36] = i + 48; %1$cchar *a = %3$c%4$s%3$c;%1$cdprintf(open(name, O_WRONLY | O_CREAT, 0644), a, 10, i - 1, 34, a);%1$csystem(i > 0 ? comp : NULL);%1$c}";
 	dprintf(open(name, O_WRONLY | O_CREAT, 0644), a, 10, i - 1, 34, a);
-	system(comp);
+	system(i > 0 ? comp : NULL);
 }
