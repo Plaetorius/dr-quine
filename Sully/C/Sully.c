1e06ce82145c1bcab64efaa4ea72d002c6b3e2a6
Tom GERNEZ <tgernez@student.42.fr>
2023-12-18 21:13:47 +0100

    add ident

--- Sully/C/Sully.c
+++ Sully/C/Sully.c
@@ -4,13 +4,11 @@
 
 int main() {
 	int i = 5;
-	char name[] = "Sully_ .c";
-	char comp[] = "clang Sully_ .c -o Sully_ ; ./Sully_ ";
-	name[6] = i + 48;
-	comp[12] = i + 48;
-	comp[25] = i + 48;
-	comp[36] = i + 48;
-	char *a = "#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <fcntl.h>%1$cint main()%1$c{%1$cint i = %2$d;%1$cchar name[] = %3$cSully_ .c%3$c;%1$cchar comp[] = %3$cclang Sully_ .c -o Sully_ ; ./Sully_ %3$c;%1$cname[6] = i + 48; comp[12] = i + 48; comp[25] = i + 48; comp[36] = i + 48; %1$cchar *a = %3$c%4$s%3$c;%1$cdprintf(open(name, O_WRONLY | O_CREAT, 0644), a, 10, i - 1, 34, a);%1$csystem(i > 0 ? comp : NULL);%1$c}";
-	dprintf(open(name, O_WRONLY | O_CREAT, 0644), a, 10, i - 1, 34, a);
-	system(i > 0 ? comp : NULL);
+	char name[snprintf(NULL, 0, "Sully_%d.c", i) + 1];
+	sprintf(name, "Sully_%d.c", i);
+	char comp[snprintf(NULL, 0, "clang Sully_%d.c -o Sully_%d ; ./Sully_%d", i, i, i) + 1];
+	sprintf(comp, "clang Sully_%d.c -o Sully_%d ; ./Sully_%d", i, i, i);	
+	char *a = "#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <fcntl.h>%1$cint main() {%1$c%5$cint i = %2$d;%1$c%5$cchar name[snprintf(NULL, 0, %3$cSully_%%d.c%3$c, i - 1) + 1];%1$c%5$csprintf(name, %3$cSully_%%d.c%3$c, i - 1);%1$c%5$cchar comp[snprintf(NULL, 0, %3$cclang Sully_%%d.c -o Sully_%%d; ./Sully_%%d%3$c, i - 1, i - 1, i - 1) + 1];%1$c%5$csprintf(comp, %3$cclang Sully_%%d.c -o Sully_%%d; ./Sully_%%d%3$c, i - 1, i - 1, i - 1);%1$c%5$cchar *a = %3$c%4$s%3$c;%1$c%5$cdprintf(open(name, O_WRONLY | O_CREAT, 0644), a, 10, i - 1, 34, a, 9);%1$c%5$csystem(i > 1 ? comp : NULL);%1$c}";
+	dprintf(open(name, O_WRONLY | O_CREAT, 0644), a, 10, i, 34, a, 9);
+	system(i > 1 ? comp : NULL);
 }
